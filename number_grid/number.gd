a644a8853f8856fbad61a6c9454bf72c47059cca
Willeujeans <112419867+Willeujeans@users.noreply.github.com>
2025-06-20 17:38:33 -0600

    Add number spawning

--- number_grid/number.gd
+++ number_grid/number.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
+const SPEED = 4
 var num_value = 0
 var is_selected = false
+var being_collected = false
 var coordinate: Vector2i = Vector2i.ZERO
+var collection_point = Vector2.ZERO
 
 
 func set_selected(state: bool):
@@ -15,7 +18,20 @@ func set_selected(state: bool):
 
 func set_value(value:int):
 	num_value = value
+	$Panel.modulate = get_custom_color(value)
 	$ValueText.set_text("[center]" + str(num_value) + "[/center]")
+
+
+func get_custom_color(value: float) -> Color:
+	value = clamp(value, -1.0, 9.0)
+	var normalized = (value + 1.0) / 10.0
+
+	if normalized < 0.5:
+		var local_t = normalized * 2.0
+		return Color.BLUE.lerp(Color.MAGENTA, local_t)
+	else:
+		var local_t = (normalized - 0.5) * 2.0
+		return Color.MAGENTA.lerp(Color.RED, local_t)
 
 
 func set_coordinate_visual(coordinate:Vector2i):
@@ -25,4 +41,27 @@ func set_coordinate_visual(coordinate:Vector2i):
 
 func collect():
 	print("im going to die now")
-	queue_free()
+	z_index += 1
+	collection_point = get_tree().get_first_node_in_group("collectionPoint").position
+	being_collected = true
+	$Area2D.set_collision_layer_value(1, true)
+	$Area2D.set_collision_mask_value(1, true)
+
+
+func _process(delta: float) -> void:
+	if(being_collected):
+		position = lerp(position, collection_point, delta * SPEED)
+		if position.distance_to(collection_point) <= 100.0:
+			being_collected = false
+			queue_free()
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	var other_object = area.get_parent()
+	if other_object.is_in_group("number"):
+		if other_object.being_collected:
+			if get_instance_id() > other_object.get_instance_id():
+				var new_num_value = num_value + other_object.num_value
+				print("NEW: ",new_num_value)
+				set_value(new_num_value)
+				other_object.queue_free()
--- tile_map_manager.gd
+++ tile_map_manager.gd
@@ -1,33 +1,16 @@
-extends TileMapLayer
+extends Node2D
 
+var tile_map_layer : TileMapLayer = null
 var tile_to_elements: Dictionary = {}
-
-
-func generate_random_number():
-	return randi_range(-1,9)
-
-
-func _ready() -> void:
-	for cell in get_used_cells():
-		var cell_number_data = generate_random_number()
-		var cell_global_position = to_global(map_to_local(cell))
-		get_cell_tile_data(cell).set_custom_data("value", cell_number_data)
-		set_cell(cell, 1, Vector2(cell_number_data, 0))
-		
-		var number_element = load("res://number_grid/number.tscn").instantiate()
-		tile_to_elements[cell] = number_element
-		number_element.set_value(cell_number_data)
-		number_element.set_coordinate_visual(cell)
-		number_element.set_global_position(cell_global_position) 
-		$PopulateTimer.start()
-		await $PopulateTimer.timeout
-		$PopulateTimer.wait_time /= 1.5
-		get_parent().add_child(number_element)
-
-
 var start_selection = Vector2.ZERO
 var end_selection = Vector2.ZERO
 var is_selecting = false
+
+
+func _ready() -> void:
+	tile_map_layer = $TileMapLayer
+	populate_cells()
+	$SelectorVisual.set_up()
 
 
 func _input(event):
@@ -40,6 +23,28 @@ func _input(event):
 			end_selection = event.position
 			var selected_cells: Array[Vector2i] = get_selected_cells(start_selection, end_selection)
 			set_cells_to_selected(selected_cells)
+
+
+func generate_random_number():
+	return randi_range(-1,9)
+
+
+func populate_cells():
+	for cell in tile_map_layer.get_used_cells():
+		var cell_number_data = generate_random_number()
+		var cell_global_position = to_global(tile_map_layer.map_to_local(cell))
+		tile_map_layer.get_cell_tile_data(cell).set_custom_data("value", cell_number_data)
+		tile_map_layer.set_cell(cell, 1, Vector2(max(0, cell_number_data), 0))
+		
+		var number_element = load("res://number_grid/number.tscn").instantiate()
+		tile_to_elements[cell] = number_element
+		number_element.set_value(cell_number_data)
+		number_element.set_coordinate_visual(cell)
+		number_element.set_global_position(cell_global_position) 
+		$PopulateTimer.start()
+		await $PopulateTimer.timeout
+		$PopulateTimer.wait_time /= 1.5
+		get_parent().add_child(number_element)
 
 
 func set_cells_to_selected(selected_cells: Array[Vector2i]):
@@ -69,10 +74,12 @@ func sum_selection(selected_cells: Array[Vector2i]):
 func collect_selection(selected_cells: Array[Vector2i]):
 	for coordinate in selected_cells:
 		if tile_to_elements.has(coordinate):
-			erase_cell(coordinate)
+			tile_map_layer.erase_cell(coordinate)
 			var cell_number_element = tile_to_elements[coordinate]
 			tile_to_elements.erase(coordinate)
 			cell_number_element.collect()
+			$CollectionBetweenTimer.start()
+			await $CollectionBetweenTimer.timeout
 
 
 func deselect(selected_cells: Array[Vector2i]):
@@ -84,8 +91,8 @@ func deselect(selected_cells: Array[Vector2i]):
 
 func get_selected_cells(start: Vector2, end: Vector2) -> Array[Vector2i]:
 	var selected_cells: Array[Vector2i] = []
-	var start_grid:Vector2i = local_to_map(to_global(start))
-	var end_grid:Vector2i = local_to_map(to_global(end))
+	var start_grid:Vector2i = tile_map_layer.local_to_map(to_global(start))
+	var end_grid:Vector2i = tile_map_layer.local_to_map(to_global(end))
 	var minx = min(start_grid.x, end_grid.x)
 	var maxx = max(start_grid.x, end_grid.x)
 	var miny = min(start_grid.y, end_grid.y)
@@ -93,7 +100,7 @@ func get_selected_cells(start: Vector2, end: Vector2) -> Array[Vector2i]:
 	
 	for x in range(minx, maxx+1):
 		for y in range(miny, maxy+1):
-			var cell = get_cell_source_id(Vector2i(x,y))
+			var cell = tile_map_layer.get_cell_source_id(Vector2i(x,y))
 			if cell != -1:
 				selected_cells.append(Vector2i(x,y))
 	return selected_cells
